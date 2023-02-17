27b16239ef7912c7a61d5ee3480224f196d66e01
PorterCat <vernikov111roma@gmail.com>
2023-02-17 20:38:46 +0700

    Laba 03 is complited

--- laba03.cpp
+++ laba03.cpp
@@ -1,50 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-#include <ctime>
 
-
-/*int main()
-{
-    float *a;
-    float *b;
-    int n;
-    printf("Enter the massive size: "); scanf("%d", &n);
-    a = new float [n];
-    b = new float [n];
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int task1();
+int task2();
+int task3();
+void swap(float* , float*);
 
 int main()
 {
-    int n, m = 0, k = 0;
+    int task;
+    scanf("%d", &task);
+    switch(task)
+    {
+        case 1:
+            task1(); break;
+        case 2:
+            task2(); break;
+        case 3:
+            task3(); break;
+    }
+}
+
+int task1()
+{
+    int j, n, m = 0, k = 0;
     int *b, *c, *d;
 
     printf("Enter the massive size: "); scanf("%d", &n);
 
     b = (int*) malloc(n * sizeof(int)); 
 
-    srand(time(NULL));
     for (int i = 0; i < n; i++)
     {
         b[i] = -50 + rand() % (50 + 50 + 1);
-
     }
 
     c = (int*) malloc(m * sizeof(int)); 
@@ -90,4 +79,137 @@ int main()
 
 
     return 0;
-} 
+}
+
+int task2()
+{
+    int n;
+    printf("Enter the massive size: "); scanf("%d", &n);
+    float *a;
+    float **b;
+
+    a = new float [n]; 
+    b = new float* [n];
+    
+    for (int i = 0; i < n; i++ ) 
+    { 
+        a[i] = 0.01 * (-1000 + rand() % (1000 + 1000 + 1));
+        b[i] = &a[i];
+    }
+
+    for (int i = 0; i < n; i++ ) 
+    { 
+        printf("%.2f ", a[i]);
+    }
+
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = i+1; j < n; j++)
+        {
+            if(*b[i] > *b[j])
+            {
+                swap(b[i], b[j]);
+            }
+        }
+    }
+
+    printf("\n");
+    for (int i = 0; i < n; i++ ) 
+    { 
+        printf("%.2f ", a[i]);
+    }
+
+    a = NULL;
+    b = NULL;
+    return 0;
+    
+}
+
+int task3()
+{
+    int n;
+    int *arr, *b;
+
+    printf("Enter the massive size: "); scanf("%d", &n);
+
+    arr = new int [n];
+    for(int i = 0; i < n; i++)
+    {
+        arr[i] = i;
+    }
+    arr[1] = 0;
+    
+    int i = 2, j;
+    while(i < n)
+    {
+        if (arr[i] != 0)
+        {
+            j = i + i;
+            while(j <= n)
+            {
+                arr[j] = 0;
+                j = j + i;
+            }
+        }
+        i = i + 1;
+    }
+
+    int n_new = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            n_new++;
+        }
+    }
+
+    b = new int[n_new];
+    j = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            b[j] = arr[i];
+            j++;
+        }
+    }
+
+    for (int i = 0; i < n_new; i++ ) 
+    { 
+        printf("%d ", b[i]);
+    }
+
+    /*int n_new = n - counter;
+    b = new int[n_new];
+
+    for(int i = 0, j = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            b[j] = arr[i];
+            j++;
+        }
+    }
+
+    printf("\n");
+    for (int i = 0; i < n_new; i++ ) 
+    { 
+        printf("%d ", b[i]);
+    }*/
+
+    return 0;
+
+}
+
+
+
+void swap(float *xp, float *yp)
+{
+    float temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+/*
+
+*/
